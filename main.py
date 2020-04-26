2598bdf59f550e9dc69fdb10ab875701b87fd0ae
CodingDoongee <ohtaedong@kaist.ac.kr>
2020-04-26 23:27:45 +0900

    Final

--- main.py
+++ main.py
@@ -29,16 +29,19 @@ def find_faces(img):
 
 fileIdx = 0
 for img_name in file_list:
-    img_bgr = cv2.imread(img_paths + img_name)
-    img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
-    rects = find_faces(img_rgb)
-    if rects[0][0][1] < rects[0][1][1] and rects[0][0][0] < rects[0][1][0]:
-        cropped_img = img_rgb[rects[0][0][1]:rects[0][1][1], rects[0][0][0]:rects[0][1][0]]
-        try:
-            cropped_img_bgr = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)
-            if cropped_img_bgr is not None:
-                cv2.imwrite('cropped_img/' + img_name, cropped_img_bgr)
-                print(img_name + ': SUCCESS.', '(', fileIdx, '/',len(file_list)-1, ')')
-        except Exception:
-            print(img_name + ': FAIL.', '(', fileIdx, '/', len(file_list)-1, ')')
+    try:
+        img_bgr = cv2.imread(img_paths + img_name)
+        img_rgb = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
+        rects = find_faces(img_rgb)
+        if rects[0][0][1] < rects[0][1][1] and rects[0][0][0] < rects[0][1][0]:
+            cropped_img = img_rgb[rects[0][0][1]:rects[0][1][1], rects[0][0][0]:rects[0][1][0]]
+            try:
+                cropped_img_bgr = cv2.cvtColor(cropped_img, cv2.COLOR_RGB2BGR)
+                if cropped_img_bgr is not None:
+                    cv2.imwrite('cropped_img/' + img_name, cropped_img_bgr)
+                    print(img_name + ': SUCCESS.', '(', fileIdx, '/', len(file_list) - 1, ')')
+            except Exception:
+                print(img_name + ': FAIL.', '(', fileIdx, '/', len(file_list) - 1, ')')
+    except Exception:
+        continue
     fileIdx = fileIdx + 1
